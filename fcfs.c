6d8514a8b0dc6c6b607733c5e393ddcf2d79d671
Geethalahari7 <160226381+Geethalahari7@users.noreply.github.com>
2025-03-05 10:30:47 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -1,28 +1,28 @@
 #include<stdio.h> 
-main() 
+int main() 
 { 
-int bt[20], wt[20], tat[20], i, n; 
-float wtavg, tatavg; 
-printf("\nEnter the number of processes -- "); 
-scanf("%d", &n); 
-for(i=0;i<n;i++) 
-{ 
-printf("\nEnter Burst Time for Process %d -- ", i); 
-scanf("%d", &bt[i]); 
-} 
-wt[0] = wtavg = 0; 
-tat[0] = tatavg = bt[0]; 
-for(i=1;i<n;i++) 
-{ 
-wt[i] = wt[i-1] +bt[i-1]; 
-tat[i] = tat[i-1] +bt[i]; 
-wtavg = wtavg + wt[i]; 
-tatavg = tatavg + tat[i]; 
-} 
-printf("\t PROCESS \tBURST TIME \t WAITING TIME\t TURNAROUND  
-TIME\n"); 
-for(i=0;i<n;i++) 
-printf("\n\t P%d \t\t %d \t\t %d \t\t %d", i, bt[i], wt[i], tat[i]); 
-printf("\nAverage Waiting Time -- %f", wtavg/n); 
-printf("\nAverage Turnaround Time -- %f", tatavg/n);
+  int i,n,bt[20], wt[20], tat[20]; 
+  float wtavg, tatavg; 
+  printf("\nEnter no of processes:"); 
+  scanf("%d", &n); 
+  printf("enter the burst out times\n"); 
+  for(i=0;i<n;i++) 
+  { 
+    printf("\nEnter the Burst Time of Process p[%d]: ", i); 
+        scanf("%d", &bt[i]); 
+  } 
+  wtavg=wt[0]= 0; 
+  tatavg= tat[0]= bt[0]; 
+  for(i=1;i<n;i++) 
+  { 
+    wt[i] = wt[i-1] +bt[i-1]; 
+    tat[i] = tat[i-1] +bt[i]; 
+    wtavg = wtavg + wt[i]; 
+    tatavg = tatavg + tat[i]; 
+  } 
+  printf("\t PROCESS \tBURST TIME \t WAITING TIME\t TURNAROUND TIME\n"); 
+  for(i=0;i<n;i++) 
+  printf("\n\t P%d \t\t %d \t\t %d \t\t %d\n", i, bt[i], wt[i], tat[i]); 
+  printf("The Average Waiting Time is : %f\n", wtavg/n); 
+  printf("The Average Turnaround Time is : %f\n", tatavg/n); 
 }
